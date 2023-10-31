b784877a85b0f8917bc8a6f74bc8403e11b0cd09
Patryk Sztuczka <patryk.sztuczka@codeagency.pl>
2023-10-31 19:02:41 +0100

    Solved 2022/1 part 1

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+fn main() {
+    let file_content = match fs::read_to_string("input.txt") {
+        Ok(f) => f,
+        Err(err) => panic!("Problem reading the file: {}", err)
+    };
+
+    let mut max_calories: u32 = 0;
+    let mut elf_calories: u32 = 0;
+
+    for line in file_content.lines() {
+        if line.len() == 0 {
+            if max_calories < elf_calories {
+                max_calories = elf_calories;
+            }
+            elf_calories = 0;
+            continue;
+        }
+
+        let line_value: u32 = line.trim().parse().unwrap();
+
+        elf_calories += line_value
+
+
+    }
+
+    println!("Max calories: {}", max_calories)
+
+}
